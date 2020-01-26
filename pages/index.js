068a8192053d2faef62b5e408f58c0840ccca0b1
Draihal <draihal.a@gmail.com>
2020-01-26 18:27:44 +0300

    Add _error page

--- pages/index.js
+++ pages/index.js
@@ -6,26 +6,31 @@ import PartnersSection from "../components/indexPage/PartnersSection";
 import ReviewsSection from "../components/indexPage/ReviewsSection";
 import fetch from 'isomorphic-unfetch';
 
-const Homepage = props => (
-    <Layout>
+
+const Homepage = ({errorCode, props}) => {
+    if (errorCode) {
+        return <Error statusCode={errorCode}/>
+    }
+    return <Layout>
         <HomeSection
             title={props.indexPage.title}
-            short_description={props.indexPage.short_description} />
+            short_description={props.indexPage.short_description}/>
         <CoursesSection
             description={props.indexPage.description}
             number_of_students={props.indexPage.number_of_students}
             number_of_groups={props.indexPage.number_of_groups}
-            number_of_teachers={props.indexPage.number_of_teachers} />
-        <TeacherSection teachers={props.indexPage.random_three_teachers} />
+            number_of_teachers={props.indexPage.number_of_teachers}/>
+        <TeacherSection teachers={props.indexPage.random_three_teachers}/>
         {/*<PartnersSection/>*/}
         <ReviewsSection reviews={props.indexPage.random_two_reviews}/>
     </Layout>
-);
+};
 
 Homepage.getInitialProps = async function() {
     const res = await fetch(`${process.env.basePath}/api/v1/pages/index/`);
+    const errorCode = res.statusCode > 200 ? res.statusCode : false;
     const indexPage = await res.json();
-    return { indexPage };
+    return { errorCode, indexPage };
 };
 
 export default Homepage;
