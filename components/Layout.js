7a1bfae6d91653ea0df5836c97b768dfa3eb2c8b
Draihal <draihal.a@gmail.com>
2020-02-16 22:40:53 +0300

    Add bootstrap viewport

--- components/Layout.js
+++ components/Layout.js
@@ -12,6 +12,7 @@ const Layout = ({ children, title, description, isAuthenticated, deauthenticate 
         Онлайн курсы
       </title>
       {description ? <meta name='description' content={description} /> : null}
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css'
               integrity='sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh'
               crossOrigin='anonymous' />
@@ -22,21 +23,21 @@ const Layout = ({ children, title, description, isAuthenticated, deauthenticate 
     <div className='content-wrapper main'>{children}</div>
 
     <Footer />
-      <script src='https://code.jquery.com/jquery-3.4.1.slim.min.js'
-              integrity='sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n'
-              crossOrigin='anonymous'> </script>
-      <script src='https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js'
-              integrity='sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo'
-              crossOrigin='anonymous'> </script>
-      <script src='https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js'
-              integrity='sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6'
-              crossOrigin='anonymous'> </script>
+    <script src='https://code.jquery.com/jquery-3.4.1.slim.min.js'
+            integrity='sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n'
+            crossOrigin='anonymous'> </script>
+    <script src='https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js'
+            integrity='sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo'
+            crossOrigin='anonymous'> </script>
+    <script src='https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js'
+            integrity='sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6'
+            crossOrigin='anonymous'> </script>
 
   </div>
 );
 
 const mapStateToProps = (state) => (
-  {isAuthenticated: !!state.authentication.token}
+{isAuthenticated: !!state.authentication.token}
 );
 
 export default connect(mapStateToProps, actions)(Layout);
