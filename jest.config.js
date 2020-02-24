7ef44c4090a1b4a4fa35149afff505742c44135e
Draihal <draihal.a@gmail.com>
2020-02-24 16:38:17 +0300

    Add unit tests for components

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,10 @@ module.exports = {
   collectCoverageFrom: [
     '**/*.{js,jsx,ts,tsx}',
     '!**/*.d.ts',
+    '!**/*.config.js',
     '!**/node_modules/**',
+    '!**/config/**',
+    '!**/coverage/**',
   ],
   setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
@@ -18,38 +21,3 @@ module.exports = {
     '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
   },
 };
-
-// module.exports = {
-//   moduleFileExtensions: [
-//     "ts",
-//     "tsx",
-//     "js"
-//   ],
-//   transform: {
-//     "^.+\\.tsx?$": "ts-jest"
-//   },
-//   testMatch: [
-//     "**/*.(test|spec).(ts|tsx)"
-//   ],
-//   globals: {
-//     "ts-jest": {
-//       useBabelrc: true,
-//       tsConfigFile: "jest.tsconfig.json"
-//     }
-//   },
-//   coveragePathIgnorePatterns: [
-//     "/node_modules/",
-//     "enzyme.js"
-//   ],
-//   setupTestFrameworkScriptFile: "<rootDir>/enzyme.js",
-//   coverageReporters: [
-//     "json",
-//     "lcov",
-//     "text",
-//     "text-summary"
-//   ],
-//   moduleNameMapper: {
-//     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/mocks.js",
-//     "\\.(css|less|scss)$": "<rootDir>/__mocks__/mocks.js"
-//   }
-// };
